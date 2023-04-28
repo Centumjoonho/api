5cf933d8153fcd31b4de2cc5de5db5f0c505303e
Centumjoonho <ghwnsgkgkgk@gmail.com>
2023-04-28 19:04:36 +0900

    2023-04-28

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
+// Express server
 const express = require('express');
+// CORS
 const cors = require('cors');
+// DB
 const { default: mongoose } = require('mongoose');
+// Model <-DB
 const User = require('./models/user');
+const Post = require('./models/post');
+// JWT
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
+// File UPLOAD
 const multer = require('multer')
 const uploadMiddleware = multer({ dest: 'uploads/' })
-
+const fs = require('fs');
+// Secret key
 const secret = 'sdgse45egsdgfs54sdsgsd45sdfs';
 
 
-const bcrypt = require('bcryptjs');
-const salt = bcrypt.genSaltSync(10);
 
 const app = express();
-
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
 app.use(express.json());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser())
 
 mongoose.connect('mongodb+srv://ghwnsgkgk:tW1hhjpLx57NkUoB@cluster0.nt2e5j0.mongodb.net/test?retryWrites=true&w=majority')
@@ -69,6 +77,7 @@ app.post('/login', async (req, res) => {
 
 app.get('/profile', (req, res) => {
     const { token } = req.cookies;
+    // credentials id get
     jwt.verify(token, secret, {}, (err, info) => {
         if (err) throw err;
         res.json(info);
@@ -79,12 +88,47 @@ app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
 })
 
+
 // createpost data.set 하고 맞춰야해 
-app.post('/post', uploadMiddleware.single('file'), (req, res) => {
-    res.json({ files: req.file });
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext
+    fs.renameSync(path, newPath)
+
+    const { token } = req.cookies;
+
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { title, summary, content } = req.body
+        const postDoc = await Post.create({
+            title: title,
+            summary: summary,
+            content: content,
+            cover: newPath,
+            author: info.id,
+        });
+
+        res.json(postDoc);
+    });
+
+});
+app.get('/post', async (req, res) => {
+    const posts = await Post.find()
+        .populate('author', ['username'])
+        .sort({ createdAt: -1 })
+        .limit(10);
+    res.json(posts)
 })
 
+app.get('/post/:id', async (req, res) => {
+    const { id } = req.params
 
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc)
+});
 
 app.listen(4000, () => {
     console.log("Server listening on port 4000")
