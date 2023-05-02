1ac560e124703bb3264fc0139cbdabd9a6eae398
Centumjoonho <ghwnsgkgk@kakao.com>
2023-05-03 02:05:23 +0900

    2023-05-03

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const salt = bcrypt.genSaltSync(10);
 const multer = require('multer')
 const uploadMiddleware = multer({ dest: 'uploads/' })
 const fs = require('fs');
+const { request } = require('http');
 // Secret key
 const secret = 'sdgse45egsdgfs54sdsgsd45sdfs';
 
@@ -115,6 +116,42 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     });
 
 });
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+
+    let newPath = null;
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        const newPath = path + '.' + ext
+        fs.renameSync(path, newPath)
+    }
+
+    const { token } = req.cookies
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, summary, content } = req.body
+        const postDoc = await Post.findById(id)
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+
+        if (!isAuthor) {
+            return res.status(400).json('you are not the author')
+
+        }
+        postDoc.title = title;
+        postDoc.summary = summary;
+        postDoc.content = content;
+        postDoc.cover = newPath ? newPath : postDoc.cover;
+
+        await postDoc.save();
+
+        res.json(postDoc);
+    });
+
+});
+
+
 app.get('/post', async (req, res) => {
     const posts = await Post.find()
         .populate('author', ['username'])
@@ -122,6 +159,8 @@ app.get('/post', async (req, res) => {
         .limit(10);
     res.json(posts)
 })
+
+
 
 app.get('/post/:id', async (req, res) => {
     const { id } = req.params
